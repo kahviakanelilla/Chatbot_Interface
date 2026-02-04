2d84085385c86ca5e329504d852b3e3b0c720466
Lotti <86312291+kahviakanelilla@users.noreply.github.com>
2026-02-04 16:59:49 +0100

    Update model configuration and add HF_TOKEN

--- config.py
+++ config.py
@@ -6,15 +6,21 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 #Get OpenAI Key
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# OpenAI-Model
-MODEL_NAME = "gpt-4o-2024-11-20"
+# API Key Hugging Face (Settings -> Access Tokens)
+HF_TOKEN = os.getenv("HF_TOKEN") 
+
+#MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
+#MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
+#MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
+MODEL_NAME = "microsoft/Phi-3-mini-4k-instruct"
 
 # Forbidden prompts (copied text)
 FORBIDDEN_PROMPTS = [
     "create five personas that represent distinct user groups",
-    "create five personas and ensure that the set of personas aligns with your understanding of diversity"
+    "create five personas and ensure that the set of personas aligns with your understanding of diversity",
+    "Hi!"
 ]
 # Define password
 PASSWORD = os.getenv("PASSWORD")
